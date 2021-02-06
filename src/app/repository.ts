9ed73f67306b7e182c7177959a6a0f9495873bcf
olesigilai <ole_sigilai@yahoo.com>
2021-02-06 16:47:52 +0300

    items of a class are removed from class repository

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -2,8 +2,6 @@ export class Repository {
     constructor(
 public avatar_url:string,
 public html_url:string,
-public followers_url:string,
-public location:string,
 public name:string
     ){ }
 }
