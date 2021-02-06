f169db5aae6fa271c912641cb97e9ed82ee8cc52
olesigilai <ole_sigilai@yahoo.com>
2021-02-06 16:46:25 +0300

    userRequest method called to assign class items

--- src/app/git.service.ts
+++ src/app/git.service.ts
@@ -4,18 +4,48 @@ import { User } from './user';
 import { Repository } from './repository';
 import { HttpClient} from '@angular/common/http';
 import { environment} from '../environments/environment'
+import { resolve } from 'dns';
+import { rejects } from 'assert';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class GitService {
-getUser(
-  username:string,
-  
+user:User;
 
-){
-  return User
-}
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.user = new User("","","",0,0,0,0,"","","",);
+   }
+   userRequest(){
+     interface ApiResponse{
+      avatar_url: any,
+      name: string,
+      bio: string,
+      public_repos: number,
+      public_gists: number,
+      followers: number,
+      following: number,
+      url: string,
+      location:string,
+      email: string
+     }
+     let promise = new Promise((resolve,reject)=>{
+       this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+         this.user.avatar_url = response.avatar_url
+        this.user.name = response.name
+        this.user.bio = response.bio
+        this.user.public_repos = response.public_repos
+        this.user.public_gists = response.public_gists
+        this.user.followers = response.followers
+        this.user.following = response.following
+        this.user.url = response.url
+        this.user.location = response.location
+        this.user.email = response.email
+      resolve()
+      })
+      error=>{}
+      
+     })
+   }
 }
