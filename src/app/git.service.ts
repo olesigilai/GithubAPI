06bc3ba8ac00563b7a5ca1f6a363c87cdb0e58c2
olesigilai <ole_sigilai@yahoo.com>
2021-02-07 20:44:16 +0300

    repository constructor is added to the service ts

--- src/app/git.service.ts
+++ src/app/git.service.ts
@@ -2,64 +2,92 @@ import { Injectable } from '@angular/core';
 import { from } from 'rxjs';
 import { User } from './user';
 import { Repository } from './repository';
-import { HttpClient} from '@angular/common/http';
-import { environment} from '../environments/environment'
-
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class GitService {
-user:User;
-userProfile:any[] = []
+  user: User;
+  userRep: Repository;
 
+  constructor(private http: HttpClient) {
+    this.user = new User('', '', '', 0, 0, 0, 0, '', '', '');
+    this.userRep = new Repository('', '', '', '');
+  }
+  userRequest(search_term: any) {
+    interface ApiResponse {
+      avatar_url: any;
+      login: string;
+      bio: string;
+      public_repos: number;
+      public_gists: number;
+      followers: number;
+      following: number;
+      url: string;
+      location: string;
+      email: string;
+     
+    }
+    let final_url =
+      environment.apiUrl + search_term + '?access_token=' + environment.apiKey;
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<ApiResponse>(final_url)
+        .toPromise()
+        .then(
+          (response) => {
+            this.user = response;
 
-  constructor(private http:HttpClient) {
-    this.user = new User("","","",0,0,0,0,"","","",);
-   }
-   userRequest(search_term:any){
-     interface ApiResponse{
-      avatar_url: any,
-      login: string,
-      bio: string,
-      public_repos: number,
-      public_gists: number,
-      followers: number,
-      following: number,
-      url: string,
-      location:string,
-      email: string
-     }
-     let final_url = environment.apiUrl+search_term+'?access_token='+environment.apiKey
-     let promise = new Promise((resolve,reject)=>{
-       this.http.get<ApiResponse>(final_url).toPromise().then(response=>{  
+            // let avatar_url = response.avatar_url
+            // let name = response.login
+            // let bio= response.bio
+            // let public_repos = response.public_repos
+            // let public_gists = response.public_gists
+            // let followers = response.followers
+            // let following = response.following
+            // let url = response.url
+            // let location = response.location
+            // let email = response.email
+            // let userr= new User(avatar_url,name,bio,public_repos,public_gists,followers,following,url,location,email)
 
-        this.user = response
-       
-         
-        // let avatar_url = response.avatar_url
-        // let name = response.login
-        // let bio= response.bio
-        // let public_repos = response.public_repos
-        // let public_gists = response.public_gists
-        // let followers = response.followers
-        // let following = response.following
-        // let url = response.url
-        // let location = response.location
-        // let email = response.email
-        // let userr= new User(avatar_url,name,bio,public_repos,public_gists,followers,following,url,location,email)
+            // this.userProfile.push(userr)
+            // console.log(this.userProfile)
+            resolve();
+          },
+          (error) => {
+            reject();
+          }
+        );
+    });
+    return promise;
+  }
 
-        // this.userProfile.push(userr)
-        // console.log(this.userProfile)
+displayRepos(user:any) {
+  interface apiResponse {
+    html_url:string,
+    name:string,
+    description:string,
+    language:string
+
+  }
+  
+
+  let url = environment.apiUrl  + user + '/repos' + '?access_token=' + environment.apiKey;
+  let promise = new Promise((resolve, reject) => {
+    this.http.get<apiResponse>(url).toPromise().then(response => {
+      this.userRep = response;
       resolve()
-      },
-       error=>{
-        reject()
-      }
-      )     
-     })
-     return promise
-   }
+
+    }, error => {
+      reject();
+    })
+
+  });
+  return promise
 }
+
+
+}
+
